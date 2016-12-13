410cd806e7d2d0d1fe4e7b955a58431fb666504e
linyang <dorisyang.sysu@gmail.com>
2016-12-13 14:53:21 -0500

    Update lighting.ino

--- lighting.ino
+++ lighting.ino
@@ -1,187 +1,195 @@
-// Wood lamp has a white light indicatior. Also, the other part change colors according to angle. The brightness is depending of the volume of the music.
-// Metal lamp has the same effect as wood lamp.
-// Acrylic lamp only has a white light indicator.
-// When two or three indicators meet each other, that two or three lamps start to bling.
-
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
 #include <avr/power.h>
 #endif
 
-#define PIN1            13
-#define PIN2            12
-#define PIN3            11
+#define PIN1            13 // wood
+#define PIN2            12 // acrylic
+#define PIN3            11 // metal
 
-#define NUMPIXEL1       30
-#define NUMPIXEL2       30
-#define NUMPIXEL3       30
+#define NUMPIXEL1       36 // wood
+#define NUMPIXEL2       40 // acrylic
+#define NUMPIXEL3       16 // metal
 
 Adafruit_NeoPixel woodLamp = Adafruit_NeoPixel(NUMPIXEL1, PIN1, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel metalLamp = Adafruit_NeoPixel(NUMPIXEL2, PIN2, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel arcylicLamp = Adafruit_NeoPixel(NUMPIXEL3, PIN3, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel acrylicLamp = Adafruit_NeoPixel(NUMPIXEL2, PIN2, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel metalLamp = Adafruit_NeoPixel(NUMPIXEL3, PIN3, NEO_GRB + NEO_KHZ800);
 
 // Encoder connect to digitalpin 2 and 3 on the Arduino.
 volatile unsigned int counter = 0; //This variable will increase or decrease depending on the rotation of encoder
 
+// Variables to control lighting in metal lamp
+int current = 0;
 int previous = 0;
-int f = 0;
+int difference = 0;
+int index = 0;
 
+// Byte sent fron MAXMSP
+int maxData = 0;
 
 void setup() {
   woodLamp.begin();
+  acrylicLamp.begin();
   metalLamp.begin();
-  arcylicLamp.begin();
 
   Serial.begin(9600);
 
-  //Setting up interrupt
-  //A rising pulse from encodenren activated ai0(). AttachInterrupt 0 is DigitalPin nr 2 on moust Arduino.
+  // Setting up interrupt
+  // A rising pulse from encodenren activated ai0(). AttachInterrupt 0 is DigitalPin nr 2 on moust Arduino.
   attachInterrupt(0, ai0, RISING);
 
-  //B rising pulse from encodenren activated ai1(). AttachInterrupt 1 is DigitalPin nr 3 on moust Arduino.
+  // B rising pulse from encodenren activated ai1(). AttachInterrupt 1 is DigitalPin nr 3 on moust Arduino.
   attachInterrupt(1, ai1, RISING);
 }
 
 void loop() {
-  int potValue1 = analogRead(A0);
-  int potValue2 = analogRead(A1);
 
-  int color1 = map(potValue1, 0, 1024, 0, 6);
-  int color2 = map(potValue2, 0, 1024, 0, 6);
-  int k = map(potValue2, 0, 1023, 0, 28);
-
-  int current = map(counter, 0, 65535, 0, 2000);
-  int l = current - previous;
-
-  // brightness for the lamps
-  int b1 = 10;
-  int b2 = 10;
-  int b3 = 10;
-
-  // arcylic lamp
-  if (l > 0) {
-    f ++;
-    if (f > 29) {
-      f = 0;
-    }
-  } else if (l < 0) {
-    f--;
-    if (f < 0) {
-      f = 29;
-    }
-  }
-  Serial.println(f);
-
-  if (f == 28) {
-    for (int p = f; p < f + 2; p++) {
-      arcylicLamp.setPixelColor(p, arcylicLamp.Color(b3, b3, b3));
-      arcylicLamp.show();
-    }
-    arcylicLamp.setPixelColor(0, arcylicLamp.Color(b3, b3, b3));
-    arcylicLamp.show();
-
-    for (int q = 1; q < f; q++) {
-      arcylicLamp.setPixelColor(q, arcylicLamp.Color(0, 0, 0));
-      arcylicLamp.show();
-    }
-  } else if ( f == 29) {
-    arcylicLamp.setPixelColor(29, arcylicLamp.Color(b3, b3, b3));
-    arcylicLamp.show();
-    for (int q = 0; q < 2; q++) {
-      arcylicLamp.setPixelColor(q, arcylicLamp.Color(b3, b3, b3));
-      arcylicLamp.show();
-    }
-    for (int q = 2; q < f; q++) {
-      arcylicLamp.setPixelColor(q, arcylicLamp.Color(0, 0, 0));
-      arcylicLamp.show();
-    }
-  } else {
-    for (int p = f; p < f + 3; p++) {
-      arcylicLamp.setPixelColor(p, arcylicLamp.Color(b3, b3, b3));
-      arcylicLamp.show();
-    }
-
-    for (int q = 0; q < f; q++) {
-      arcylicLamp.setPixelColor(q, arcylicLamp.Color(0, 0, 0));
-      arcylicLamp.show();
-    }
-
-    for (int s = f + 3; s < NUMPIXEL3; s++) {
-      arcylicLamp.setPixelColor(s, arcylicLamp.Color(0, 0, 0));
-      arcylicLamp.show();
-    }
+  while (Serial.available() > 0) {
+    maxData = Serial.read();
   }
 
+  // brightness for neopixel
+  int b1 = maxData; // wood
+  int b2 = maxData; // acrylic
+  int b3 = 100; // metal
+
+  int woodPot = analogRead(A0);
+  int arylicPot = analogRead(A1);
+
+  int woodCol = map(woodPot, 0, 1024, 0, 6);
+  int arylicCol = map(arylicPot, 0, 1024, 0, 6);
+
+  current = map(counter, 0, 65535, 0, 1000);
+  difference = current - previous;
   previous = current;
 
-  // metal lamp
-  for (int n = k; n < k + 3; n++) {
-    metalLamp.setPixelColor(n, metalLamp.Color(b2, b2, b2));
-    metalLamp.show();
-  }
+  // send data to MAXMSP
+  int encoderValue = map(counter, 0, 65535, 0, 1023);
+  Serial.print(woodCol, DEC);
+  Serial.print(" ");
+  Serial.print(arylicCol, DEC);
+  Serial.print(" ");
+  Serial.println(encoderValue);
 
-  for (int m = 0; m < k; m++) {
-    if (color2 == 0) {
-      metalLamp.setPixelColor(m, metalLamp.Color(0, b1 / 5 * 0 , b1 / 5 * 5));
-    } else if (color2 == 1) {
-      metalLamp.setPixelColor(m, metalLamp.Color(0, b1 / 5 * 1 , b1 / 5 * 4));
-    } else if (color2 == 2) {
-      metalLamp.setPixelColor(m, metalLamp.Color(0, b1 / 5 * 2 , b1 / 5 * 3));
-    } else if (color2 == 3) {
-      metalLamp.setPixelColor(m, metalLamp.Color(0, b1 / 5 * 3 , b1 / 5 * 2));
-    } else if (color2 == 4) {
-      metalLamp.setPixelColor(m, metalLamp.Color(0, b1 / 5 * 4 , b1 / 5 * 1));
-    } else {
-      metalLamp.setPixelColor(m, metalLamp.Color(0, b1 / 5 * 5 , b1 / 5 * 0));
-    }
-    metalLamp.show();
-  }
 
-  for (int g = k + 3; g < NUMPIXEL2; g++) {
-    if (color2 == 0) {
-      metalLamp.setPixelColor(g, metalLamp.Color(0, b1 / 5 * 0 , b1 / 5 * 5));
-    } else if (color2 == 1) {
-      metalLamp.setPixelColor(g, metalLamp.Color(0, b1 / 5 * 1 , b1 / 5 * 4));
-    } else if (color2 == 2) {
-      metalLamp.setPixelColor(g, metalLamp.Color(0, b1 / 5 * 2 , b1 / 5 * 3));
-    } else if (color2 == 3) {
-      metalLamp.setPixelColor(g, metalLamp.Color(0, b1 / 5 * 3 , b1 / 5 * 2));
-    } else if (color2 == 4) {
-      metalLamp.setPixelColor(g, metalLamp.Color(0, b1 / 5 * 4 , b1 / 5 * 1));
-    } else {
-      metalLamp.setPixelColor(g, metalLamp.Color(0, b1 / 5 * 5 , b1 / 5 * 0));
-    }
-    metalLamp.show();
-  }
-
-  // wood lamp
+  // lighting for the wood lamp
   for (int i = 0; i < 3; i++) {
-    woodLamp.setPixelColor(i, woodLamp.Color(b1, b1, b1));
+    woodLamp.setPixelColor(i, woodLamp.Color(100, 100, 100));
     woodLamp.show();
   }
-
-  for (int j = 3; j < NUMPIXEL1; j++) {
-    if (color1 == 0) {
-      woodLamp.setPixelColor(j, woodLamp.Color(b1 / 5 * 5, b1 / 5 * 5 , 0));
-    } else if (color1 == 1) {
-      woodLamp.setPixelColor(j, woodLamp.Color(b1 / 5 * 4, b1 / 5 * 5 , 0));
-    } else if (color1 == 2) {
-      woodLamp.setPixelColor(j, woodLamp.Color(b1 / 5 * 3, b1 / 5 * 5 , 0));
-    } else if (color1 == 3) {
-      woodLamp.setPixelColor(j, woodLamp.Color(b1 / 5 * 2, b1 / 5 * 5 , 0));
-    } else if (color1 == 4) {
-      woodLamp.setPixelColor(j, woodLamp.Color(b1 / 5 * 1, b1 / 5 * 5 , 0));
+  for (int i = 3; i < NUMPIXEL1; i++) {
+    if (woodCol == 0) {
+      woodLamp.setPixelColor(i, woodLamp.Color(0, b1 / 5 * 5, b1 / 5 * 0));
+    } else if (woodCol == 1) {
+      woodLamp.setPixelColor(i, woodLamp.Color(0, b1 / 5 * 3, b1 / 5 * 2));
+    } else if (woodCol == 2) {
+      woodLamp.setPixelColor(i, woodLamp.Color(0, b1 / 5 * 1, b1 / 5 * 4));
+    } else if (woodCol == 3) {
+      woodLamp.setPixelColor(i, woodLamp.Color(0, b1 / 5 * 0, b1 / 5 * 5));
+    } else if (woodCol == 4) {
+      woodLamp.setPixelColor(i, woodLamp.Color(0, b1 / 5 * 2, b1 / 5 * 3));
     } else {
-      woodLamp.setPixelColor(j, woodLamp.Color(b1 / 5 * 0, b1 / 5 * 5 , 0));
+      woodLamp.setPixelColor(i, woodLamp.Color(0, b1 / 5 * 4, b1 / 5 * 1));
     }
     woodLamp.show();
   }
 
+
+  // lighting for the acrylic lamp
+  int k = map(arylicPot, 0, 1023, 0, NUMPIXEL2 - 2);
+  for (int i = k; i < k + 3; i++) {
+    acrylicLamp.setPixelColor(i, acrylicLamp.Color(100, 100, 100));
+    acrylicLamp.show();
+  }
+  for (int i = 0; i < k; i++) {
+    if (arylicCol == 0) {
+      acrylicLamp.setPixelColor(i, acrylicLamp.Color(b2 / 5 * 5, 0, b2));
+    } else if (arylicCol == 1) {
+      acrylicLamp.setPixelColor(i, acrylicLamp.Color(b2 / 5 * 3, 0, b2));
+    } else if (arylicCol == 2) {
+      acrylicLamp.setPixelColor(i, acrylicLamp.Color(b2 / 5 * 1, 0, b2));
+    } else if (arylicCol == 3) {
+      acrylicLamp.setPixelColor(i, acrylicLamp.Color(b2 / 5 * 0, 0, b2));
+    } else if (arylicCol == 4) {
+      acrylicLamp.setPixelColor(i, acrylicLamp.Color(b2 / 5 * 2, 0, b2));
+    } else {
+      acrylicLamp.setPixelColor(i, acrylicLamp.Color(b2 / 5 * 4, 0, b2));
+    }
+    acrylicLamp.show();
+  }
+  for (int i = k + 3; i < NUMPIXEL2; i++) {
+    if (arylicCol == 0) {
+      acrylicLamp.setPixelColor(i, acrylicLamp.Color(b2 / 5 * 5, 0, b2));
+    } else if (arylicCol == 1) {
+      acrylicLamp.setPixelColor(i, acrylicLamp.Color(b2 / 5 * 3, 0, b2));
+    } else if (arylicCol == 2) {
+      acrylicLamp.setPixelColor(i, acrylicLamp.Color(b2 / 5 * 1, 0, b2));
+    } else if (arylicCol == 3) {
+      acrylicLamp.setPixelColor(i, acrylicLamp.Color(b2 / 5 * 0, 0, b2));
+    } else if (arylicCol == 4) {
+      acrylicLamp.setPixelColor(i, acrylicLamp.Color(b2 / 5 * 2, 0, b2));
+    } else {
+      acrylicLamp.setPixelColor(i, acrylicLamp.Color(b2 / 5 * 4, 0, b2));
+    }
+    acrylicLamp.show();
+  }
+
+
+  // lighting for the metal lamp
+  if (difference > 0) {
+    index ++;
+    if (index > NUMPIXEL3 - 1) {
+      index = 0;
+    }
+  } else if (difference < 0) {
+    index--;
+    if (index < 0) {
+      index = 15;
+    }
+  }
+
+  if (index == NUMPIXEL3 - 2) {
+    for (int i = index; i < index + 2; i++) {
+      metalLamp.setPixelColor(i, metalLamp.Color(b3, b3, b3));
+      metalLamp.show();
+    }
+    metalLamp.setPixelColor(0, metalLamp.Color(b3, b3, b3));
+    metalLamp.show();
+    for (int i = 1; i < index; i++) {
+      metalLamp.setPixelColor(i, metalLamp.Color(0, 0, 0));
+      metalLamp.show();
+    }
+  } else if ( index == NUMPIXEL3 - 1) {
+    metalLamp.setPixelColor(NUMPIXEL3 - 1, metalLamp.Color(b3, b3, b3));
+    metalLamp.show();
+    for (int i = 0; i < 2; i++) {
+      metalLamp.setPixelColor(i, metalLamp.Color(b3, b3, b3));
+      metalLamp.show();
+    }
+    for (int i = 2; i < index; i++) {
+      metalLamp.setPixelColor(i, metalLamp.Color(0, 0, 0));
+      metalLamp.show();
+    }
+  } else {
+    for (int i = index; i < index + 3; i++) {
+      metalLamp.setPixelColor(i, metalLamp.Color(b3, b3, b3));
+      metalLamp.show();
+    }
+    for (int i = 0; i < index; i++) {
+      metalLamp.setPixelColor(i, metalLamp.Color(0, 0, 0));
+      metalLamp.show();
+    }
+    for (int i = index + 3; i < NUMPIXEL3; i++) {
+      metalLamp.setPixelColor(i, metalLamp.Color(0, 0, 0));
+      metalLamp.show();
+    }
+  }
 
   delay(10);
 }
 
 
+// Encoder
 void ai0() {
   // ai0 is activated if DigitalPin nr 2 is going from LOW to HIGH
   // Check pin 3 to determine the direction
@@ -201,9 +209,5 @@ void ai1() {
     counter++;
   }
 }
-
-
-
-
 
 
